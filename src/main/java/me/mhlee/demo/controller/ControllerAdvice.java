b33718792850315a5b83af586929b09e35188c7f
ifthenelse <jigul.sw@gmail.com>
2024-08-05 13:57:17 +0900

    Exception 핸들러 추가

--- src/main/java/me/mhlee/demo/controller/ControllerAdvice.java
+++ src/main/java/me/mhlee/demo/controller/ControllerAdvice.java
@@ -9,10 +9,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    private static final String DEFAULT_ERROR_MESSAGE = "알수없는 에러가 발생 했습니다.";
+
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationError(MethodArgumentNotValidException ex) {
         var message = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
 
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<String> handleExceptionHandler(Exception ex) {
+        return new ResponseEntity<>(DEFAULT_ERROR_MESSAGE, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
