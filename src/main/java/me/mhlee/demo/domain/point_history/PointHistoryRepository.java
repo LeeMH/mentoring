4537dfb1f69939496161e6e503029d66396c724c
ifthenelse <jigul.sw@gmail.com>
2024-08-07 09:55:39 +0900

    point, history 도메인 추가

---
+++ src/main/java/me/mhlee/demo/domain/point_history/PointHistoryRepository.java
@@ -0,0 +1,6 @@
+package me.mhlee.demo.domain.point_history;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
+}
